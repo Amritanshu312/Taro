5801343275b41e23b2eb10f48f6917df6f2a84b6
Amritanshu312 <ffdarkrayon@gmail.com>
2024-08-15 14:51:06 +0530

    [] fix🎉: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['s4.anilist.co', "i.ytimg.com", 'artworks.thetvdb.com', 'media.kitsu.io', 'media.kitsu.app', 'kitsu-production-media.s3.us-west-002.backblazeb2.com', 'media.themoviedb.org'],
+    domains: [
+      's4.anilist.co',
+      "i.ytimg.com",
+      'artworks.thetvdb.com',
+      'media.kitsu.io',
+      'media.kitsu.app',
+      'kitsu-production-media.s3.us-west-002.backblazeb2.com',
+      'media.themoviedb.org'
+    ],
+    unoptimized: true
   }
 };
 
