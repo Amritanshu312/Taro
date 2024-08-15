f6d735fa148f2e2b353284e0fd54a4171b4ced80
Amritanshu312 <ffdarkrayon@gmail.com>
2024-08-15 16:36:19 +0530

    [] feat🎉: Added website in google search console

--- app/layout.js
+++ app/layout.js
@@ -81,6 +81,10 @@ export const metadata = {
     title: "Taro : Stream Anime Free Without Those Annoying Ads",
     description: "Welcome to Taro, your ultimate destination for streaming anime free of charge and without any annoying ads. Dive into a vast collection of your favorite anime series and movies, all available in high quality. Enjoy a seamless viewing experience with zero interruptions, and explore new titles every day. At Taro, your anime adventure awaits!",
   },
+  other: {
+    'google-site-verification': 'ls1OUoOoLjxYsmKMPQ1ML9P99TWDsm7d5hfnGQjW7Tw',
+    "X-Frame-Options": "SAMEORIGIN",
+  }
 };
 
 
