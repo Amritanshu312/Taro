8313d3262b9bb097e81b081450456b9916ae41f0
Amritanshu312 <ffdarkrayon@gmail.com>
2024-09-16 15:32:04 +0530

    Revert "[see-all] feat🎉: Linked it to continue watching page"
    This reverts commit bdb0f3a1df19fd708906fa48977256562da7c9e2.

--- content/Home/WatchHistory.jsx
+++ content/Home/WatchHistory.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import ContinueWatchingCard from "@/components/Cards/ContinueWatchingCard/ContinueWatchingCard";
 import { FaArrowRight } from "react-icons/fa";
 import { getWatchProgress } from "@/utils/GetProgress";
-import Link from "next/link";
 
 const WatchHistory = () => {
   const [mappedData, setMappedData] = useState([]);
@@ -22,7 +21,7 @@ const WatchHistory = () => {
       <div className="flex justify-between">
         <h1 className="text-[#f6f4f4ea] font-medium text-2xl font-['poppins'] max-[450px]:text-[1.2rem]">| Continue Watching</h1>
 
-        <Link href={"/continue-watching"} className="text-[#ffffffbd] flex items-center gap-1 cursor-pointer hover:text-slate-500 transition">See All <FaArrowRight /></Link>
+        <div className="text-[#ffffffbd] flex items-center gap-1 cursor-pointer hover:text-slate-500 transition">See All <FaArrowRight /></div>
       </div>
 
       <div className="mt-8 mb-24 grid grid-cols-[repeat(auto-fit,minmax(343px,1fr))] max-[725px]:grid-cols-[repeat(auto-fit,minmax(285px,1fr))] gap-3">
